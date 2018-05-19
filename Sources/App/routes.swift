6e6912d6122f9393c5774de74e7eaa4ed503bb6c
larms <larms0@163.com>
2018-05-20 00:37:55 +0800

    添加Acronym模型

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,12 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        return try req.content.decode(Acronym.self).flatMap(to: Acronym.self, { acronym in
+            return acronym.save(on: req)
+        })
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
