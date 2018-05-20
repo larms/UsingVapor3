bfc9329f5d3e28821bd7744b3f6807afd7f35a6e
larms <larms0@163.com>
2018-05-20 19:26:56 +0800

    Add database password

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -23,9 +23,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     
     // Configure a PostgreSQL database
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, username: username, database: databaseName)
+    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, username: username, database: databaseName, password: password)
     let psql = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: psql, as: .psql)
     services.register(databases)
