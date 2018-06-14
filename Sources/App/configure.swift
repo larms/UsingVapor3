228e4a14ec5025de51f51ad1b049d0adea298fce
larms <larms0@163.com>
2018-06-14 14:40:20 +0800

    前一次提交忘记打开数据库密码的代码了

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -45,11 +45,11 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     }
     
     // https://github.com/vapor/fluent-postgresql/issues/9
-//    let password = Environment.get("DATABASE_PASSWORD") ?? "password" // 设置密码后Xcode运行会崩溃, 部署的时候再使用密码
-//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password" // 设置密码后Xcode运行会崩溃, 部署的时候再使用密码
+    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
     
     // Configure a PostgreSQL database
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName)
+//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName)
     
     let psql = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: psql, as: .psql)
